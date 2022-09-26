54c0dd8092cb844d4f605f8c785ffef5d97f25ba
김재만 <daga4242@gmail.com>
2022-09-26 21:26:21 +0900

    22.09.26

---
+++ answer.py
@@ -0,0 +1,50 @@
+answers = {
+    "a1" : "",
+    "a2" : "",
+    "a3" : "",
+    "a4" : "",
+    "a5" : "",
+    "a6" : "",
+    "a7" : "",
+    "a8" : "",
+    "a9" : "",
+    "a10" : "",
+    "a11" : "",
+    "a12" : "",
+    "a13" : "",
+    "a14" : "",
+    "a15" : "",
+    "a16" : "",
+}
+
+answers["a1"] = ""
+
+answers["a2"] = ""
+
+answers["a3"] = ""
+
+answers["a4"] = ""
+
+answers["a5"] = ""
+
+answers["a6"] = ""
+
+answers["a7"] = ""
+
+answers["a8"] = ""
+
+answers["a9"] = ""
+
+answers["a10"] = ""
+
+answers["a11"] = ""
+
+answers["a12"] = ""
+
+answers["a13"] = ""
+
+answers["a14"] = ""
+
+answers["a15"] = ""
+
+answers["a16"] = ""
