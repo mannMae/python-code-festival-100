6c732c0ccb88dfe27485b8b8e92a6ff48d34c7e6
김재만 <daga4242@gmail.com>
2022-09-28 20:15:44 +0900

    /usr/bin/python3 /Users/jae-mankim/Desktop/Linkerverse/project/test/python-100/question.py

--- answer.py
+++ answer.py
@@ -1,50 +1,70 @@
+from question import QuestionVals
+
+def a1():
+    QuestionVals.q1_nums.pop()
+    QuestionVals.q1_nums.pop()
+    return 
+
+def a2():
+    return 
+
+def a3():
+    return 
+
+def a4():
+    return
+
+def a5():
+    return
+
+def a6():
+    return
+
+def a7():
+    return
+
+def a8():
+    return
+
+def a9():
+    return
+
+def a10():
+    return
+
+def a11():
+    return
+
+def a12():
+    return
+
+def a13():
+    return
+
+def a14():
+    return
+
+def a15():
+    return
+
+def a16():
+    return
+
 answers = {
-    "a1" : "",
-    "a2" : "",
-    "a3" : "",
-    "a4" : "",
-    "a5" : "",
-    "a6" : "",
-    "a7" : "",
-    "a8" : "",
-    "a9" : "",
-    "a10" : "",
-    "a11" : "",
-    "a12" : "",
-    "a13" : "",
-    "a14" : "",
-    "a15" : "",
-    "a16" : "",
+    "a1" : a1,
+    "a2" : a2,
+    "a3" : a3,
+    "a4" : a4,
+    "a5" : a5,
+    "a6" : a6,
+    "a7" : a7,
+    "a8" : a8,
+    "a9" : a9,
+    "a10" : a10,
+    "a11" : a11,
+    "a12" : a12,
+    "a13" : a13,
+    "a14" : a14,
+    "a15" : a15,
+    "a16" : a16,
 }
-
-answers["a1"] = ""
-
-answers["a2"] = ""
-
-answers["a3"] = ""
-
-answers["a4"] = ""
-
-answers["a5"] = ""
-
-answers["a6"] = ""
-
-answers["a7"] = ""
-
-answers["a8"] = ""
-
-answers["a9"] = ""
-
-answers["a10"] = ""
-
-answers["a11"] = ""
-
-answers["a12"] = ""
-
-answers["a13"] = ""
-
-answers["a14"] = ""
-
-answers["a15"] = ""
-
-answers["a16"] = ""
--- question.py
+++ question.py
@@ -1,26 +1,4 @@
-# from answer import answers
-
-#===========================================
-
-answers = {
-    "a1" : "",
-    "a2" : "",
-    "a3" : "",
-    "a4" : "",
-    "a5" : "",
-    "a6" : "",
-    "a7" : "",
-    "a8" : "",
-    "a9" : "",
-    "a10" : "",
-    "a11" : "",
-    "a12" : "",
-    "a13" : "",
-    "a14" : "",
-    "a15" : "",
-    "a16" : "",
-}
-
+from answer import answers
 
 
 # 1. 다음 리스트에서 400, 500을 삭제하는 코드를 입력하세요.
@@ -59,58 +37,27 @@ print(q5_a + q5_b)
 
 #===========================================
 
-q1 = {"nums" : [100,200,300,400,500]}
+QuestionVals ={
+    q1_nums : q1_nums
+}
 
-
-
-answers["a1"] = ""
-
-answers["a2"] = ""
-
-answers["a3"] = ""
-
-answers["a4"] = ""
-
-answers["a5"] = ""
-
-answers["a6"] = ""
-
-answers["a7"] = ""
-
-answers["a8"] = ""
-
-answers["a9"] = ""
-
-answers["a10"] = ""
-
-answers["a11"] = ""
-
-answers["a12"] = ""
-
-answers["a13"] = ""
-
-answers["a14"] = ""
-
-answers["a15"] = ""
-
-answers["a16"] = ""
 
 
 def check_answer():
     print("1번문제", answers["a1"])
-    assert q1_nums == [100,200,300] or answers["a1"] == [100,200,300] or answers["a1"] =="", "1번문제 오답입니다."
+    # assert q1_nums == [100,200,300], "1번문제 오답입니다."
 
-    print("2번문제", answers["a2"])
-    assert q2_l == [200,100,1000,300] or answers["a2"] == [200,100,1000,300] or answers["a2"] =="", "2번문제 오답입니다."
+    # print("2번문제", answers["a2"])
+    # assert q2_l == [200,100,1000,300] or answers["a2"] == [200,100,1000,300] or answers["a2"] =="", "2번문제 오답입니다."
 
-    print("3번문제", answers["a3"])
-    assert answers["a3"] =="list" or answers["a3"] =="", "3번문제 오답입니다."
+    # print("3번문제", answers["a3"])
+    # assert answers["a3"] =="list" or answers["a3"] =="", "3번문제 오답입니다."
 
-    print("4번문제", answers["a4"])
-    assert answers["a4"] =="3" or answers["a4"] ==3 or answers["a4"] =="", "4번문제 오답입니다."
+    # print("4번문제", answers["a4"])
+    # assert answers["a4"] =="3" or answers["a4"] ==3 or answers["a4"] =="", "4번문제 오답입니다."
 
-    print("5번문제", answers["a5"])
-    assert answers["a5"] =="4" or answers["a5"] ==4 or answers["a5"] == "16" or answers["a5"] == 16 or answers["a5"] =="", "5번문제 오답입니다."
+    # print("5번문제", answers["a5"])
+    # assert answers["a5"] =="4" or answers["a5"] ==4 or answers["a5"] == "16" or answers["a5"] == 16 or answers["a5"] =="", "5번문제 오답입니다."
 
 check_answer()
 
