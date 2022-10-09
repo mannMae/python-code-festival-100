3f3baf50072ac665955be37f7335f5e0003efc4e
Jaeman Kim <daga4242@gmail.com>
2022-10-09 15:32:50 +0900

    22.10.09_1

---
+++ test copy 2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def largestRectangleArea(self, heights: list[int]) -> int:
+        height = 0
+        maxWidth = 0
+        while sum(heights):
+            height +=1
+            for i, j in enumerate(heights):
+                heights
+                maxWidth = max(maxWidth, )
+                if i:
+                    heights[j] -=1
+        return maxWidth
+
+a = Solution
+
+a.largestRectangleArea([1,2,3])
--- test copy.py
+++ test copy.py
@@ -1,1 +1,15 @@
-print([] is True)
+def solution(citations):
+    citations.sort(reverse=True)
+    answer = 0
+    is_breaked = False
+    
+    for i, j in enumerate(citations):
+        if i+1 > j:
+            answer = i
+            is_breaked = True
+            break
+            
+    if is_breaked == False:
+        answer = len(citations)
+
+    return answer
