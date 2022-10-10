e33bb47386d4aeb6f267458b1826a2ddfa4adf4c
Jaeman Kim <daga4242@gmail.com>
2022-10-10 22:12:57 +0900

    test

--- test.js
+++ test.js
@@ -1,1 +1,2 @@
-console.log([]);
+str = 'sadbutsad';
+needle = 'sad';
--- test_.py
+++ test_.py
@@ -1,1 +1,22 @@
-print("".join([1,2,3]))
+import collections
+
+q1 = [3,2,7,2]
+q2 = [4,6,8,1]
+count = 0
+
+queue = collections.deque
+q1_history = []
+
+while sum(q1) != sum(q2):
+    
+    if q1 in q1_history:
+        continue
+    
+    q1_history.append(q1)
+
+    queue.append([q1, q2, count])
+    
+    
+
+
+print(count)
